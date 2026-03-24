1a79e5cc84fc8d8437e2b43e4f75da201eff23c8
Pavithra <shinypavithra.addanki@gmail.com>
2026-03-24 18:01:01 +0530

    Change server listen address to '0.0.0.0'

--- server/index.js
+++ server/index.js
@@ -233,9 +233,9 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
     const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    );
+    server.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
   })
   .catch((err) => {
     console.error("MongoDB connection failed:", err.message);
